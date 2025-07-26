0587923495b239752fe88d18be45a2bded849c7a
Afcyare <fabdim7@gmail.com>
2025-07-26 11:08:46 +0300

    fixed height

--- scripts/filtred-temples.js
+++ scripts/filtred-temples.js
@@ -124,7 +124,7 @@ function createTempleCard(filteredTemples) {
         img.setAttribute("alt", `${temple.templeName}`);
         img.setAttribute("loading", "lazy");
         img.setAttribute("width", "400");
-        img.setAttribute("hight", "250");
+        img.setAttribute("height", "250");
 
         card.appendChild(name);
         card.appendChild(location);
